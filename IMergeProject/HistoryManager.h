ee84bf3d36998cb231c494090659c6fc3f6e1b79
Akhilesh <akhileshzmishra@gmail.com>
2014-09-12 13:06:23 +0530

    Update HistoryManager.h

--- IMergeProject/HistoryManager.h
+++ IMergeProject/HistoryManager.h
@@ -4,35 +4,46 @@
 #include "CommonHeader.h"
 #include "HistoryData.h"
 
+namespace XLHistory
+{
 
-void InsertHistory(HistDataPair& histPair);
-HistDataPair* RemoveRecentHistory();
+void InsertHistory(XLHistoryData::HistDataPair& histPair);
+XLHistoryData::HistoryDataSPtr RemoveRecentHistory();
 void ClearHistory();
 void EnableHistory(bool set);
+int RemainingHistory();
+
 
 class CHistoryManager
 {
-	static CHistoryManager* m_ManagerInstance;
-	CHistoryData            mHistoryData;
-	bool                    mbRecord;
+	static CHistoryManager*                m_ManagerInstance;
+	XLHistoryData::CHistoryData            mHistoryData;
+	bool                                   mbRecord;
 private:
 	CHistoryManager(void);
 	CHistoryManager(const CHistoryManager& );
 
 public:
 	static CHistoryManager* GetInstance(); 
-	void RecordHistory(HistDataPair& data); 
-	HistDataPair* GetRecentData();
+	void RecordHistory(XLHistoryData::HistDataPair& data); 
+	XLHistoryData::HistoryDataSPtr GetRecentData();
 
 	void Enable(bool set)
 	{
 		mbRecord = set;
 	}
+	void SetMaxHistory(int size)
+	{
+		mHistoryData.SetHistoryMaxSize(size);
+	}
+
+	int CurrentHistorySize();
 
 	void Clear();
 	~CHistoryManager(void);
 };
 
+}; //namespace XLHistory
 
 
 
