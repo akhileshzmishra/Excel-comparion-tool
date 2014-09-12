4b3bda60703af76749f2a97c33c5e7781fe8ceaf
Akhilesh <akhileshzmishra@gmail.com>
2014-09-12 13:14:47 +0530

    Update CommonHeader.h

--- IMergeProject/CommonHeader.h
+++ IMergeProject/CommonHeader.h
@@ -1,5 +1,5 @@
-#ifndef _COMMONHEADER_H1_
-#define _COMMONHEADER_H1_
+#ifndef _COMMONHEADER_H1g_
+#define _COMMONHEADER_H1g_
 
 #include <stdio.h>
 #include "XLCommonHeader.h"
@@ -7,6 +7,7 @@
 #include <map>
 #include <list>
 #include "IDualMap.h"
+#include "LinkedList.h"
 
 #define ZERO_TABLE_X1 09
 #define ZERO_TABLE_X2 30
@@ -19,7 +20,7 @@
 #define TABLE_HEIGHT 400
 #define CELL_HEIGHT 30
 #define GAP_DEFAULT 20
-#define LOAD_BTN_W 30
+#define LOAD_BTN_W 35
 #define LOAD_BTN_H	FIRST_TABLE_Y
 
 #define DIFF_POINTER_BUTTON_X  20
@@ -38,6 +39,10 @@
 
 #ifndef YELLOWCOL
 #define YELLOWCOL RGB(125,125,0)
+#endif
+
+#ifndef ALICEBLUE
+#define ALICEBLUE RGB(240,248,255)
 #endif
 
 #ifndef LEVENDERBLUSHCOL
@@ -71,7 +76,7 @@
 #define INVALIDROW	 -1
 #define INVALIDCOL   -1
 
-#define MAX_HISTORY_SIZE 100
+#define MAX_HISTORY_SIZE 200
 
 typedef pair<int, int>    ComparedDataPair;	 
 typedef	vector<ComparedDataPair >  CompareListType;
@@ -106,6 +111,7 @@ public:
 	friend class ColCompareItr;
 
 	RowComparison(): Row1(0), Row2(0){}
+	RowComparison(int r1, int r2): Row1(r1), Row2(r2){}
 	void Insert(int c1, int c2);
 	bool FindC1(int c2, int& ret);
 	bool FindC2(int c1, int& ret);
@@ -126,20 +132,21 @@ public:
 
 class RowComparisonList
 {
-	typedef std::map<int, int> RowPos;
-	typedef RowPos::iterator   RowItr;
-	typedef std::pair<int, int> RowPair; 
-	RowPos                     mFirstRowPos;
-	RowPos                     mSecondRowPos;
-	typedef std::vector<RowComparison> RowCompList;
-	RowCompList                mRowList;
-	int                        mFill;
+	typedef LListNode<RowComparison>     Node;
+	typedef std::map<int, Node*>         RowPos;
+	typedef RowPos::iterator             RowItr;
+	typedef std::pair<int, Node*>        RowPair; 
+	RowPos                               mFirstRowPos;
+	RowPos                               mSecondRowPos;
+	typedef LinkedList<RowComparison>    RowCompList;
+	RowCompList                          mRowList;
+	int                                  mFill;
 	friend class RowComparisonIterator;
 public:
 	RowComparisonList();
 	~RowComparisonList();
-	int size()              {return (int)mRowList.size();}
-	void resize(int size);
+	int size()              {return (int)mRowList.Size();}
+
 
 	RowComparison* Insert(int r1, int r2);
 	RowComparison* AtLastFill();
@@ -149,18 +156,16 @@ public:
 	RowComparison* FindRow1(int r1);
 	RowComparison* FindRow2(int r2);
 
-	int FindIndex1(int r1);
-	int FindIndex2(int r2);
-
-	void clear();
+	void Clear();
+private:
+	Node* GetRow1Node(int r1);
+	Node* GetRow2Node(int r2);
 
 };
 
-class RowComparisonIterator
+class RowComparisonIterator: public LinkedListItr<RowComparison>
 {
-	int mitr;
 	RowComparisonList* mList;
-	RowComparison*     mThisValue;
 public:
 	RowComparisonIterator(RowComparisonList* mlist = 0);
 	~RowComparisonIterator();
@@ -168,22 +173,33 @@ public:
 	bool IsEnd();
 	bool IsBegin();
 	bool IsValid();
-	void Reset(bool toLast = false);
-	void operator ++();
-	void operator ++(int);
-	void operator --();
-	void operator --(int);
-	RowComparison* GetValue()   {return mThisValue;}
-	void DeleteCurrValue();
-	void MoveToIndex(int placeIdx);
 
-	bool Next()                     {return ((mitr + 1) < mList->mFill);}
-	bool Previous()                 {return ((mitr - 1) >= 0);}
+	RowComparison* GetValue()   
+	{
+		return Value();
+	}
+
+	void DeleteCurrValue();
+	bool MoveToRow(int row, bool firstIndex);
+
+	void ResetToFront();
+	void ResetToBack();
+
+	bool Next()
+	{
+		return HasNext();
+	}
+
+	bool Previous()
+	{
+		return HasPrevious();
+	}
 };
 
 
 enum XLTableParam
 {
+	XLTableParamReloadTable,
 	XLTableParamFullTable,
 	XLTableParamDiffTable,
 	XLTableParamSameTable,
